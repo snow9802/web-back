07d62a33de89c18e2d0bdec826770c17f185b82d
jaycode <seizethemoment099@gmail.com>
2024-11-13 14:34:07 +0900

    Initial Commit

---
+++ src/main/java/com/scsa/moin_back/MoinBackApplication.java
@@ -0,0 +1,13 @@
+package com.scsa.moin_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoinBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoinBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scsa/moin_back/MoinBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scsa.moin_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoinBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
